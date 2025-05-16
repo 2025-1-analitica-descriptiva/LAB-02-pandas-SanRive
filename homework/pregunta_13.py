e8f2864631ce6b84e00498a5c6c4eb01bd55b4f2
Santiago Rivera Mejia <sriverame@unal.edu.co>
2025-05-16 11:52:50 -0500

    Workshop #2

--- homework/pregunta_13.py
+++ homework/pregunta_13.py
@@ -5,7 +5,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
-
+import pandas as pd
 def pregunta_13():
     """
     Si la columna `c0` es la clave en los archivos `tbl0.tsv` y `tbl2.tsv`,
@@ -20,3 +20,9 @@ def pregunta_13():
     E    275
     Name: c5b, dtype: int64
     """
+    table0 = pd.read_csv('files/input/tbl0.tsv', sep='\t')
+    table2 = pd.read_csv('files/input/tbl2.tsv', sep='\t')    
+    merged = pd.merge(table0, table2, on='c0')
+    result = merged.groupby('c1')['c5b'].sum()
+    
+    return result
